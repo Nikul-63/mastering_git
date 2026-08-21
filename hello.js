b3fe5fb1cc482fed95a5391138ec794eb5dfb1ed
Dabhi Nikul <125889984+Nikul-63@users.noreply.github.com>
2026-08-21 12:03:14 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
 console.log("Hello This is Dabhi Nikul..!");
 const a = 5, b = 5;
 console.log("Sum of ", a, " and ", b, " is: ", (a+b));
+
+console.log("Hello Dabhi Nikul You Are The Best Programmer In The World..!")
