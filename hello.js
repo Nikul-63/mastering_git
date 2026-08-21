3873248c8dab144a810b17858fb357fa4d027288
Dabhi Nikul <125889984+Nikul-63@users.noreply.github.com>
2026-08-21 12:15:40 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -8,3 +8,5 @@ console.log("Subtraction of ", a, " and ", b, " is: ", (a-b));
 console.log("Multiplication of ", a, " and ", b, " is: ", (a*b));
 console.log("Division of ", a, " and ", b, " is: ", (a/b));
 console.log("Hello Mr.Govind Dabhi You Are The Best..!");
+
+console.log("I am currently learning jenkins as a ci/cd pipeline tool..!");
