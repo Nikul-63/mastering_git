0069440368a14b893a85f10b69c57407df09fd10
Dabhi Nikul <125889984+Nikul-63@users.noreply.github.com>
2026-08-21 12:13:05 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,3 +5,4 @@ console.log("Sum of ", a, " and ", b, " is: ", (a+b));
 console.log("Hello Dabhi Nikul You Are The Best Programmer In The World..!")
 
 console.log("Subtraction of ", a, " and ", b, " is: ", (a-b));
+console.log("Multiplication of ", a, " and ", b, " is: ", (a*b));
